de9ec0a3aa792c44e80fe546fe603f3a71140b29
Charindu Thisara <114144341+CharinduThisara@users.noreply.github.com>
2022-12-22 15:58:05 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,58 @@
+var express = require('express');
+const dotenv = require('dotenv');
+
+var apiController = require("./API/API");
+// var authController = require("./RouteFacade/authController");
+
+// var {RestoreSession} = require("./MODEL/Authentication");
+
+
+
+var app = express();
+dotenv.config();
+
+// RestoreSession();
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json())
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Allow-Methods", "DELETE, PUT");
+    next();
+});
+
+app.use('/api',apiController);
+// app.use('/auth',authController);
+
+app.listen(process.env.port);
+
+
+/* async function BackendStart(){
+    
+    var app = express();
+    dotenv.config();
+
+    RestoreSession();
+
+    app.use(express.urlencoded({extended: true}));
+    app.use(express.json())
+
+    
+    app.use(function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+        res.header("Access-Control-Allow-Methods", "DELETE");
+        next();
+    });
+
+    app.use('/api',apiController);
+    app.use('/auth',authController);
+
+    app.listen(process.env.port);
+
+    return(app);
+}
+
+module.exports = {BackendStart} */

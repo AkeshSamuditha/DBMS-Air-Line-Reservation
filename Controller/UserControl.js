7610f6a4be1bf3c99cc461a46c360265ea7d7049
Charindu Thisara <114144341+CharinduThisara@users.noreply.github.com>
2022-12-28 02:15:30 +0530

    Update UserControl.js

--- Controller/UserControl.js
+++ Controller/UserControl.js
@@ -15,8 +15,23 @@ class UserControl{
     async getFlights(method,user){
         try{
             const body = method.getBody();
-            // const count = body.count;
-            const data = await executeSQL(`SELECT * FROM flights`);
+            const From = body.From;
+            const To = body.To;
+            const From_Date = body.From_Date;
+            const To_Date = body.To_Date;
+            
+            const data = await executeSQL(
+
+            `Select Date_of_travel, Dep_time, Arr_time, Tickets_Remaining 
+             From Flights 
+             where Route = (
+                 Select Route_ID 
+                 From Routes 
+                 Where Origin_ID = '${From}' AND Destination_ID = '${To}' AND (Date_of_travel BETWEEN '${From_Date}' AND '${To_Date}')
+                 order by Date_of_travel
+                 ) AND Tickets_remaining > 0;`
+                
+            );
             return(data);
         }catch(err){
             return err;

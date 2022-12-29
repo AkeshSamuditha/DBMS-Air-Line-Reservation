d43e3a712972ce5934a4cfe8699267910a1031e4
Charindu Thisara <114144341+CharinduThisara@users.noreply.github.com>
2022-12-30 04:54:55 +0530

    Update index.js
    Seems to be done

--- index.js
+++ index.js
@@ -2,16 +2,14 @@ var express = require('express');
 const dotenv = require('dotenv');
 
 var apiController = require("./API/API");
-// var authController = require("./RouteFacade/authController");
+var authController = require("./API/Auth");
 
-// var {RestoreSession} = require("./MODEL/Authentication");
-
-
+var {RestoreSession} = require("./MODEL/Authentication");
 
 var app = express();
 dotenv.config();
 
-// RestoreSession();
+RestoreSession();
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json())
@@ -24,35 +22,6 @@ app.use(function(req, res, next) {
 });
 
 app.use('/api',apiController);
-// app.use('/auth',authController);
+app.use('/auth',authController);
 
 app.listen(process.env.port);
-
-
-/* async function BackendStart(){
-    
-    var app = express();
-    dotenv.config();
-
-    RestoreSession();
-
-    app.use(express.urlencoded({extended: true}));
-    app.use(express.json())
-
-    
-    app.use(function(req, res, next) {
-        res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
-        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-        res.header("Access-Control-Allow-Methods", "DELETE");
-        next();
-    });
-
-    app.use('/api',apiController);
-    app.use('/auth',authController);
-
-    app.listen(process.env.port);
-
-    return(app);
-}
-
-module.exports = {BackendStart} */

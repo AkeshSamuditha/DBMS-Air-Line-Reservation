d9f414094bd4ee8ebc4d18e9c3746a42506f2cd3
Charindu Thisara <114144341+CharinduThisara@users.noreply.github.com>
2023-01-06 02:21:10 +0530

    Update Auth.js
    minor changes

--- API/Auth.js
+++ API/Auth.js
@@ -4,8 +4,9 @@ var router = express.Router();
 const {ResponseHandler} = require("../Controller/ResponseController");
 const Method = require("../Controller/method");
 
-const {login,register,ExtractUser} = require("../MODEL/Authentication");
+const {login,register} = require("../MODEL/Authentication");
 
+//Request No: 07
 router.post('/login',async function(req, res){
     console.log("login");
     var method = new Method(req,res);
@@ -16,7 +17,9 @@ router.post('/login',async function(req, res){
 
 });
 
-router.post('/register',async function(req, res){
+
+// Request No: 08
+router.post('/Register',async function(req, res){
     console.log("register");
     var method = new Method(req,res);
     
@@ -26,31 +29,5 @@ router.post('/register',async function(req, res){
 
 });
 
+
 module.exports = router;
-
-
-/* 
-
-
-const session = require('express-session');
-
-router.use(session({
-    secret: 'secret',
-    cookie: { maxAge: 60000 },
-    saveUninitialized: false
-}));
-
-router.use((req, res, next) => {
-    console.log(`${req.method} - ${req.url}`);
-    next();
-});
-
-router.post('/login',async function(req, res){
-
-    var method = new Method(req,res);
-    
-    var status = login(method);
-    //res.status(ResponseHandler(status)).send(status);
-
-});
- */

1fed9d59f38f5c892603d1d68c883b72d6f7042e
TheNobleStag <tharukavi31@gmail.com>
2023-01-14 07:47:09 +0530

    uybcnaiuxz

--- Client/src/App.js
+++ Client/src/App.js
@@ -16,6 +16,7 @@ import AdminLogIn from "./components/pages/AdminLogIn";
 import GuestUser from "./components/pages/GuestUser";
 import SeatReservation from "./components/pages/SeatReservation";
 import Payment from "./components/pages/Payment";
+import Test from "./components/pages/Test";
 
 function App() {
   return (
@@ -35,6 +36,7 @@ function App() {
           <Route path="/guest-user" component={GuestUser} />
           <Route path="/seatReservations" component={SeatReservation} />
           <Route path="/payments" component={Payment} />
+          <Route path="/test" component={Test} />
         </Switch>
         <Footer />
       </Router>
